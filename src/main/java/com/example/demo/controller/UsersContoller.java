bdc97fdc50c4e6f4c9900e3fcc1c9720a5f7a9e6
Admin <Admin@LAPTOP-30A3S4TS>
2024-01-16 17:31:45 +0530

    1st version of TuneHub

---
+++ src/main/java/com/example/demo/controller/UsersContoller.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entities.Users;
+import com.example.demo.services.UsersService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class UsersContoller {
+	
+	@Autowired
+	UsersService repo;
+	
+	@PostMapping("/register")
+	public String addUsers(@ModelAttribute Users user)
+	{
+		boolean user_status=repo.emailExits(user.getEmail());
+		if(user_status==false) {
+		repo.addUser(user);
+		}
+		else {
+			System.out.println("User is already present");
+		}
+		return "home";
+	}
+	@PostMapping("/validate")
+	public String validate(@RequestParam("email") String email,@RequestParam("password") String password,
+			HttpSession session) {
+		if(repo.validateUser(email,password)==true) {
+		String role=repo.getRole(email);
+		
+		session.setAttribute("email",email);
+		if(role.equals("admin")) {
+			return "adminHome";
+		}
+		else {
+			return "customerHome";
+			}
+		}
+		else {
+			return "login";
+		}
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "login";
+	}
+}
